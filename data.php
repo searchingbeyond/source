7fd8114161567bd59e1eb281b2a96f2c920f1b1c
searchingbeyond <1355036599@qq.com>
2018-03-11 11:15:10 +0800

    初步完成页面的内容，网站的链接为文字形式

---
+++ data.php
@@ -0,0 +1,17 @@
+<?php
+	$str = array(
+		"https://chuanke.baidu.com/"=>"百度传课",
+		"https://ke.qq.com/"=>"腾讯课堂",
+		"https://www.icourse163.org"=>"中国大学MOOC",
+		"https://mooc.guokr.com/"=>"mooc学院",
+		"https://www.imooc.com/"=>"慕课网",
+		"http://www.xuetangx.com/"=>"学堂在线",
+		"http://www.mooc.cn/"=>"慕课中国",
+		"https://www.shiyanlou.com/"=>"实验楼",
+		"http://study.163.com/"=>"网易云课堂",
+		"http://www.51cto.com/"=>"51CTO",
+		"https://www.csdn.net/"=>"CSDN",
+		"https://www.oschina.net/"=>"开源中国"
+    );
+	echo json_encode($str);
+?>
---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+window.onload=function(){ 
+    var xhr = new XMLHttpRequest(); 
+    xhr.onreadystatechange =function(){ 
+        if(xhr.readyState == 4){ 
+            var source = JSON.parse(xhr.responseText); 
+            var str = ""; 
+            for( var k in source ){ 
+                str += "<li><a href='" + k + "'>" + source[k] + "</a></li>"; 
+            }
+            var body = document.getElementsByTagName("body")[0]; 
+            body.style.margin = "20% 40%"; 
+            body.innerHTML = "<ol>" + str + "</ol>"; 
+        } 
+    } 
+    xhr.open("GET","data.php",true); 
+    xhr.send(); 
+}
